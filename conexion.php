7a19952325b04eece313147f7f6c7da2392c3ced
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-04-14 19:10:39 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","chaneldb");
+$conex = mysqli_connect("grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","wdyyidfspu0cfjpz","xitqd8qe6uw2ab67","kcyuzhli19g60bde");
 
 ?>
